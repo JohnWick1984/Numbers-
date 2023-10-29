43224896c8375d6a1f71bef89f87b0f298585055
Евгений <@EUGENE1984>
2023-10-29 12:52:49 +0500

    Добавьте файлы проекта.

---
+++ Градусы/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите операцию:");
+        Console.WriteLine("1. Перевести из Фаренгейта в Цельсий");
+        Console.WriteLine("2. Перевести из Цельсия в Фаренгейт");
+
+        int choice;
+        if (int.TryParse(Console.ReadLine(), out choice))
+        {
+            double temperature, convertedTemperature;
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Введите температуру в Фаренгейтах: ");
+                    if (double.TryParse(Console.ReadLine(), out temperature))
+                    {
+                        convertedTemperature = (temperature - 32) * 5 / 9;
+                        Console.WriteLine($"Температура в Цельсиях: {convertedTemperature:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: Введено некорректное значение температуры.");
+                    }
+                    break;
+                case 2:
+                    Console.Write("Введите температуру в Цельсиях: ");
+                    if (double.TryParse(Console.ReadLine(), out temperature))
+                    {
+                        convertedTemperature = (temperature * 9 / 5) + 32;
+                        Console.WriteLine($"Температура в Фаренгейтах: {convertedTemperature:F2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: Введено некорректное значение температуры.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка: Выбрана неверная операция.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: Введена некорректная операция.");
+        }
+    }
+}
---
+++ ДЗ1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число от 1 до 100: ");
+        int number;
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            if (number >= 1 && number <= 100)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: число вне диапазона от 1 до 100");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введено некорректное число");
+        }
+    }
+}
